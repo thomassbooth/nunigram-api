d15126cb644786c06cf67cf0ab9464d47253a9fd
Thomas Booth <thomasbooth26@gmail.com>
2023-01-26 23:20:29 +0000

    Setup stuff

--- backend/nunigram/models.py
+++ backend/nunigram/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+# class post(models.Model):
+
+#     owner = models.ForeignKey()
+#     location = models.CharField()
+#     likes = models.IntegerField()
+#     comments = models.JSONField()
+
